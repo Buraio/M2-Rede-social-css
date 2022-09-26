8f077b64a57f52e8ed05bdec97258b5097c74a94
Bryan <bryanbatista79688@gmail.com>
2022-09-25 23:19:14 -0300

    Modified/created archives

--- script/database/index.js
+++ script/database/index.js
@@ -1,2 +1,19 @@
-/* Desenvolva a lógica da página aqui */
+const btnModal = document.querySelectorAll('[data-modal-control]');
+console.log(btnModal)
 
+for (let i = 0; i < btnModal.length; i++) {
+
+  btnModal[i].addEventListener('click', ()=> {
+  
+    let event = btnModal[i].getAttribute('data-modal-control');
+    console.log(event);
+
+    console.log(btnModal[i].querySelector(event))
+
+    console.log()
+  
+  })
+
+}
+
+// Ainda em construção!!
